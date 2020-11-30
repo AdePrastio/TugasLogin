1f918395edff178e72f51b799fa07e3aede9d85c
AdePrastio <adeeprastio@gmail.com>
2020-11-30 09:21:00 +0700

    Menambahkan Baris

--- Date.php
+++ Date.php
@@ -1,4 +1,14 @@
 <?php 
 
 	$myemail	= "AdePrastio@gmail.com";
-	$mypass		= "17062017"; ?>
+	$mypass		= "17062017";
+
+	if (isset($_POST['login'])) {
+		$email 	= $_POST['email'];
+		$pass 	= $_POST['password'];
+		if ( $email == $myemail and $pass == $mypass ) {
+			if ( isset($_POST['remember'])  ) {
+				 setcookie('email', $email, time()+60*60*7);
+				 setcookie('pass', $pass, time()+60*60*7);
+			}
+			 ?>
