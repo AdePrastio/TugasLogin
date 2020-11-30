3290e8f0f870a6ebaac3bb0203f10e21fe911a84
AdePrastio <adeeprastio@gmail.com>
2020-11-30 09:11:35 +0700

    Menambahkan Baris

--- Login.php
+++ Login.php
@@ -6,3 +6,12 @@
 		text-align: center;
 	}
 </style>
+<table cellpadding="5" cellspacing="10" align="center">
+	<h3>Login From menggunakan session dan cookies dengan Remember Me</h3>
+	<form method="post" action="date.php">
+		<tr><th>Email</th><td><input type="text" id="email" name="email"></td></tr>
+		<tr><th>Password</th><td><input type="password" id="pass" name="password"></td></tr>
+		<tr><td colspan="2" align="center"><input type="checkbox" name="remember" value="1">Remember Me</td></tr>
+		<tr><td colspan="2" align="right"><input type="submit" value="Login" name="login"></td></tr>
+	</form>
+</table>
